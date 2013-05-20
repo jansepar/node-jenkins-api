ee15293f3624ffd5000c51c6ef10564a4fcae4fc
Shawn Jansepar <shawn@mobify.com>
2013-05-20 01:55:57 -0700

    Adding grunt to make releasing easier!

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  grunt.loadNpmTasks('grunt-release');
+
+};
